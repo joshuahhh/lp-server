6f2bd132fa7bf5d92becefaeb7de85f63ba5760d
Joshua Horowitz <joshuah@alum.mit.edu>
2024-03-12 12:16:08 -0700

    commit

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { AutomergeUrl, Repo } from "@automerge/automerge-repo";
 import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket";
 import cors from "cors";
 import debugLib from "debug";
-import express from "express";
+import express, { Request, Response } from "express";
 import https from "node:https";
 import fsP from "node:fs/promises";
 import * as child_process from "node:child_process";
@@ -110,14 +110,41 @@ async function main() {
     }
   });
 
-  app.get("/", async (req, res) => {
-    const dockerResult = await exec("docker container list");
-
+  async function handleStatusPage(req: Request, res: Response) {
     const workerInfosHtml =
       JSON.stringify(Array.from(workerInfos.values()), null, 2)
         .replaceAll(/automerge:.{28}/g, (match) =>
           `<a href="https://joshuahhh.com/amview/#/${match}">${match}</a>`
         );
+
+    const dockerContainerResult = await exec("docker container list");
+
+    const dockerImageResult = await exec("docker image list");
+
+    const workerLogsHtmls = await Promise.all(
+      Array.from(workerInfos.values()).map(async ({ dockerContainerName }) => {
+        const logFileName = `${dockerContainerName}.log`;
+        try {
+          await exec(`docker cp ${dockerContainerName}:/app/index.log ${logFileName}`);
+          const logContents = await fsP.readFile(logFileName, "utf8");
+          return `
+            <h3>${dockerContainerName}</h3>
+            <pre>${logContents}</pre>
+          `;
+        } catch (e) {
+          return `
+            <h3>${dockerContainerName}</h3>
+            <pre style="color: red">${e}</pre>
+          `;
+        } finally {
+          try {
+            await fsP.unlink(logFileName);
+          } catch (e) {
+            debug(`error unlinking ${logFileName}:`, e);
+          }
+        }
+      })
+    );
 
     res.contentType("text/html");
     res.send(`
@@ -127,18 +154,59 @@ async function main() {
           <title>lp-server</title>
         </head>
         <body>
-          <h1>lp-server</h1>
-          probably running ok! on commit ${(await exec('git rev-parse HEAD')).stdout.trim().slice(0, 7)}
-          <h2>workerInfos</h2>
+          <h1><pre>lp-server</pre></h1>
+          <p>probably running ok! on commit ${(await exec('git rev-parse HEAD')).stdout.trim().slice(0, 7)}</p>
+          <p><a href="/log">lp-server service log</a></p>
+          <h2><pre>workerInfos</pre></h2>
           <div>  <!-- keep json chrome extension off our case -->
             <pre>${workerInfosHtml}</pre>
           </div>
-          <h2>"docker container list"</h2>
-          <pre>${dockerResult.stdout}</pre>
-          <pre style="color: red">${dockerResult.stderr}</pre>
+          <h2><pre>docker container list</pre></h2>
+          <pre>${dockerContainerResult.stdout}</pre>
+          <h2><pre>docker image list</pre></h2>
+          <pre>${dockerImageResult.stdout}</pre>
+          <pre style="color: red">${dockerContainerResult.stderr}</pre>
+          <h2><pre>worker logs</pre></h2>
+          ${workerLogsHtmls.join("\n")}
         </body>
       </html>
     `);
+  }
+
+  app.get("/", async (req, res) => {
+    await handleStatusPage(req, res);
+  });
+
+  app.get("/log", async (req, res) => {
+    res.contentType("text/html");
+    let response = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>lp-server service log</title>
+        </head>
+        <body>
+          <h1><pre>lp-server service log</pre></h1>
+          <p><a href="/">back</a></p>
+    `;
+    try {
+      const journalctlResult = await exec("journalctl -u lp-server.service -b");
+
+      res.contentType("text/html");
+      response += `
+        <pre>${journalctlResult.stdout}</pre>
+        <pre style="color: red">${journalctlResult.stderr}</pre>
+      `;
+    } catch (e) {
+      response += `
+        <pre style="color: red">${e}</pre>
+      `;
+    }
+    response += `
+        </body>
+      </html>
+    `;
+    res.send(response);
   });
 
   const PORT = 8088;
